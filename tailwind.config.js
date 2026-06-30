8ffaa87d082b5ff3d9634bd176067da190525286
agungset0641 <8703389@gmail.com>
2026-06-30 11:20:26 +0700

    local css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'coastal-bg': '#F8FAFC',
+        'coastal-text': '#0F172A',
+        'coastal-accent': '#334155',
+      },
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif'],
+        'inter': ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
